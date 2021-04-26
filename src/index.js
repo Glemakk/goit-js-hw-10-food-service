430a6a2d228358afc5f735654d247b966f658453
Glemakk <kovgar.brand@gmail.com>
2021-04-27 01:01:29 +0200

    Создана шаблонизация

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+import menu from './menu.json';
 import './styles.css';
+import menuCardTpl from './templates/menu-cards.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+    checkbox: document.querySelector('#theme-switch-toggle'),
+    menu: document.querySelector('.js-menu'),
+}
+
+const menuMarkup = createMenuCardsMarkup(menu);
+refs.menu.insertAdjacentHTML('beforeend', menuMarkup)
+
+function createMenuCardsMarkup(menu) {
+    return menu.map(menuCardTpl).join('');
+}
+
+//refs.checkbox.addEventListener('change', )
